1b15eeb694d61c247e589ab6cf7784582abada6e
kishore-koii <108358596+kishore-koii@users.noreply.github.com>
2023-02-20 20:03:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    ibm = {
+	  # source is mandatory for community providers
+      source = "IBM-Cloud/ibm" 
+      version = "1.13.1"
+    }
+  }
+}
 ##############################################################################
 # IBM Cloud Provider
 ##############################################################################
